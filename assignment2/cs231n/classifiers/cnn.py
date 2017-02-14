f572596494e18a3eb095f978c9b0538567ae0bbb
Manvel Avetisian <avetisian@gmail.com>
2017-02-14 07:51:25 +0300

    Finished Assignment 2.

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -132,6 +132,105 @@ class ThreeLayerConvNet(object):
     ############################################################################
     
     return loss, grads
-  
-  
+
+
+class UberConvNet(object):
+  """
+  The network operates on minibatches of data that have shape (N, C, H, W)
+  consisting of N images, each with height H and width W and with C input
+  channels.
+  """
+
+  def __init__(self, input_dim=(3, 32, 32), num_filters=32, filter_size=7,
+               hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0,
+               dtype=np.float32):
+    """
+    Initialize a new network.
+
+    Inputs:
+    - input_dim: Tuple (C, H, W) giving size of input data
+    - num_filters: Number of filters to use in the convolutional layer
+    - filter_size: Size of filters to use in the convolutional layer
+    - hidden_dim: Number of units to use in the fully-connected hidden layer
+    - num_classes: Number of scores to produce from the final affine layer.
+    - weight_scale: Scalar giving standard deviation for random initialization
+      of weights.
+    - reg: Scalar giving L2 regularization strength
+    - dtype: numpy datatype to use for computation.
+    """
+    self.params = {}
+    self.reg = reg
+    self.dtype = dtype
+
+    (C, H, W) = input_dim
+
+    # image size does not change by conv layer, but it get's shrinked 2x by maxpool
+    # 1 + (H + 2 * (filter_size - 1) / 2 - filter_size) / 1 = H
+    # 1 + (W + 2 * (filter_size - 1) / 2 - filter_size) / 1 = H
+
+    self.params["W1"] = weight_scale * np.random.randn(num_filters, C, filter_size, filter_size)
+    self.params["b1"] = np.zeros(num_filters)
+    self.params["W2"] = weight_scale * np.random.randn(num_filters * H * W / 4, hidden_dim)
+    self.params["b2"] = np.zeros(hidden_dim)
+    self.params["gamma2"] = np.zeros(hidden_dim)
+    self.params["beta2"] = np.zeros(hidden_dim)
+    self.params["W3"] = weight_scale * np.random.randn(hidden_dim, num_classes)
+    self.params["b3"] = np.zeros(num_classes)
+
+    for k, v in self.params.iteritems():
+      self.params[k] = v.astype(dtype)
+
+    self.bn_params2 = {'mode': 'train'}
+
+  def loss(self, X, y=None):
+    """
+    Evaluate loss and gradient for the three-layer convolutional network.
+
+    Input / output: Same API as TwoLayerNet in fc_net.py.
+    """
+    W1, b1 = self.params['W1'], self.params['b1']
+    W2, b2 = self.params['W2'], self.params['b2']
+    gamma2, beta2 = self.params['gamma2'], self.params['beta2']
+    W3, b3 = self.params['W3'], self.params['b3']
+
+    # pass conv_param to the forward pass for the convolutional layer
+    filter_size = W1.shape[2]
+    conv_param = {'stride': 1, 'pad': (filter_size - 1) / 2}
+
+    # pass pool_param to the forward pass for the max-pooling layer
+    pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
+
+    scores = None
+
+    (x1, cache1) = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
+    (x2, cache2) = affine_batchnorm_relu_forward(x1, W2, b2, gamma2, beta2, self.bn_params2)
+    (x3, cache3) = affine_forward(x2, W3, b3)
+    scores = x3
+
+    if y is None:
+      return scores
+
+    (loss, loss_dx) = softmax_loss(scores, y)
+
+    loss += 0.5 * self.reg * np.sum(W1 ** 2)
+    loss += 0.5 * self.reg * np.sum(W2 ** 2)
+    loss += 0.5 * self.reg * np.sum(W3 ** 2)
+
+    (dx2, dw3, db3) = affine_backward(loss_dx, cache3)
+    (dx1, dw2, db2, dgamma2, dbeta2) = affine_batchnorm_relu_backward(dx2, cache2)
+    (dx, dw1, db1) = conv_relu_pool_backward(dx1, cache1)
+
+    grads = {}
+    grads["W1"] = dw1 + self.reg * W1
+    grads["W2"] = dw2 + self.reg * W2
+    grads["W3"] = dw3 + self.reg * W3
+    grads["b1"] = db1
+    grads["b2"] = db2
+    grads["b3"] = db3
+    grads["gamma2"] = dgamma2
+    grads["beta2"] = dbeta2
+
+    return loss, grads
+
+
 pass
